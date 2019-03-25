ade9fbc91776898607371feae99c3141435525eb
YingHongYuan <yinghong.yuan@sjsu.edu>
2019-03-25 10:49:13 -0700

    1

--- p5/p5-exercise-1/sketch.js
+++ p5/p5-exercise-1/sketch.js
@@ -2,12 +2,14 @@ function setup(){
   createCanvas(500,500);
   background(0,250,0);
 
+// draw race car body
   nostroke();
   stroke(255);
   strokeWeight(5);
   fill(40,50,250);
-  rext(50,300,50,30);
+  rect(mouseX,mouseY,50,30);
 
+//draw race car wheels
   fill(0);
   ellipse(60,350,20,20);
   ellipse(90,350,20,20);
