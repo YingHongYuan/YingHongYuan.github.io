7c79e7ae4651adaa192abb957b81c42ea66d28d7
YingHongYuan <yinghong.yuan@sjsu.edu>
2019-03-27 11:47:54 -0700

    1

--- p5/p5-exercise-1/sketch.js
+++ p5/p5-exercise-1/sketch.js
@@ -2,39 +2,11 @@ function setup(){
   createCanvas(500,500);
   background(0,250,300);
 
-
-   if(frameCount > 180) {
-  vroom.freq(carX);
-
-  if(mouseIsPressed && mouseX > 0 && mouseX < 50 && mouseY > 0 && mouseY < 50) {
-    RaceStarted = true;
-  }
-  if(carX > 500) {
-      carX = -50
-  } else {
-    //carX = carX + 3;
-  }
-
-// draw race car body
-  stroke(255);
-  strokeWeight(5);
-  fill(40,50,250);
-  rect(mouseX,mouseY,50,30);
-  }
-
   function draw() {
-
-
-    background(0, 255,0,120);
-}
-
-//draw race car wheels
-  fill(0);{
-  ellipse(60,350,20,20);
-  ellipse(90,350,20,20);
-}
-
-if (false && true){
-  console.log("truth");
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
   }
-}
