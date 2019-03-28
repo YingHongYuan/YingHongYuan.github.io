f3d5a26aca5ffede7b50684027f987d6e7a992b8
YingHongYuan <yinghong.yuan@sjsu.edu>
2019-03-27 23:01:05 -0700

    1

--- p5/p5-exercise-1/sketch.js
+++ p5/p5-exercise-1/sketch.js
@@ -1,13 +1,19 @@
-function setup(){
-  {
-    loadImage('Lottery_Wheel-512.png', img => {
-    image(img, -1, 0);
-  });
-  createCanvas(500,500);
+
+
+function setup() {
+  createCanvas(500, 500);
   background(0,250,300);
-}
+  ellipse(252, 252, 214, 222);
+  loadImage('clock.png', img => {
+    image(img, 142, 142);
+  });
 }
 
-function draw(){
-  ellipse(255, 233, 233, 233);
+function draw() {
+  line(253, 256, mouseX, mouseY);
+  for (let x=0; x < width; x++) {
+    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
+    stroke(noiseVal*255);
+    line(x, mouseY+noiseVal*80, x, height);
+  }
 }
