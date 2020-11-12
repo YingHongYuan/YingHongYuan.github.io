ba86208273001e473679ba98f371ab32b691a04a
L A R K <lark.buckingham@sjsu.edu>
2020-11-11 17:43:31 -0800

    changes from our meeting

--- Pages/webcam/sketch.js
+++ Pages/webcam/sketch.js
@@ -1,13 +1,25 @@
 var video;
-
+let webcamDefaultImg;
 
 function setup() {
-  createCanvas(0, 0);
+
+  let cnv = createCanvas(0, 0);
+  webcamDefaultImg = select("#webcamDefault");
+
   background(0);
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO, videoLoaded);
   video.size(320,240);
+  video.parent("webcam");
+
+  console.log(video);
 }
 
 function draw() {
+
+}
+
+function videoLoaded(){
+  console.log("loaded");
+  webcamDefaultImg.hide();
 
 }
