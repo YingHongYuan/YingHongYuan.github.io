da5fcb04d019c7b6405934373951b52b984790d4
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-11-13 23:37:02 -0800

    1

--- webcam/sketch.js
+++ webcam/sketch.js
@@ -1,13 +1,29 @@
 var video;
 
+let webcamDefaultImg;
 
-function setup() {
+
+function setup() {	function setup() {
   createCanvas(0, 0);
-  background(0);
-  video = createCapture(VIDEO);
-  video.size(320,240);
-}
+  let cnv = createCanvas(0, 0);
+  webcamDefaultImg = select("#webcamDefault");
 
-function draw() {
+  background(0);	  background(0);
+  video = createCapture(VIDEO);	  video = createCapture(VIDEO, videoLoaded);
+  video.size(320,240);	  video.size(320,240);
+  video.parent("webcam");
+
+  console.log(video);
+}	}
+
+
+function draw() {	function draw() {
+
+
+}	}
+
+function videoLoaded(){
+  console.log("loaded");
+  webcamDefaultImg.hide();
 
 }
